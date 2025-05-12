0d8eea5046d8422eccad79acc59deb549bafdf5a
ahcreative <anashamid891@gmail.com>
2025-05-12 23:59:43 +0500

    new

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -4,8 +4,7 @@ function Header() {
   return (
     <nav className="bg-[#1C1C1C] py-3 ">
       <div className="container mx-auto ">
-        <img src="/logo.png" className="h-18 ml-10 md:block hidden" alt="" />
-        <img src="/icon.png" className="h-18 ml-10 md:hidden block" alt="" />
+        <img src="/logo.png" className="h-18 ml-2 block " alt="" />
       </div>
     </nav>
   );
